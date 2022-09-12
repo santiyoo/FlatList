3e2fc85913c5fa6394537fc2480ae328f99c6d5e
santiyoo <yoopy2705@gmail.com>
2022-09-12 10:14:57 -0300

    terminado

--- App.js
+++ App.js
@@ -1,17 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { FlatList } from 'react-native-web';
-import Example from './components/Example';
-import HomeScreen from './navigation/screens/Home';
 import Start from './navigation/stacks/HomeStackScreen';
 
 export default function App() {
   return (
-    // <View style={styles.container}>
-    //   <StatusBar style="auto" />
-    //   <Example/>
-    // </View>
-
     <Start/>
   );
 }
--- navigation/screens/Lista.js
+++ navigation/screens/Lista.js
@@ -1,9 +1,42 @@
-import React from 'react'
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
-import Example from '../../components/Example';
+import React from 'react';
+import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar } from 'react-native';
+import { jugadores } from '../../data/Jugadores';
 
-export default function ListaScreen(){
-    return (
-        <Example index={"index"}/>
-    )
+const Lista = () => {
+  return (
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.title}>Jugadores</Text>
+        <FlatList
+          data={jugadores}
+          renderItem={({item})=>
+            <>
+              <View>
+                <Text style={styles.subhead}>- {item.Nombre} {item.Apellido}</Text>
+              </View>
+            </>
+          }
+          keyExtractor={item => item.Apellido}
+        />
+      </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 35,
+    fontWeight: 'bold',
+    marginBottom: 15
+  },
+  subhead: {
+    fontSize: 25,
+    marginLeft: 15,
+    marginVertical: 5
+  }
+});
+
+export default Lista;
